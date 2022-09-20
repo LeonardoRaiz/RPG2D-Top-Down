c566aa66d18ce2f1a1944f693be755f463d276b4
Léo Raiz <LeonardoRaiz@icloud.com>
2022-09-20 13:41:06 -0300

    Update

---
+++ Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [Header("Components")]
+    public GameObject dialogueObj;
+    public Image profileSprite;
+    public Text speechText;
+    public Text actorNameText;
+
+    [Header("Settings")]
+    public float typingSpeed;
+
+    //Variáveis de controle
+    private bool isShowing; //se a janela está visivel?
+    private int index;
+    private string[] sentences;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+
+    }
+
+    public void Speech(string[] txt)
+    {
+        if(!isShowing)
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+    public float dialogueRange;
+    public LayerMask playerLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+        if(hit != null)
+        {
+            Debug.Log("asdasdsadasdsdas");
+        } else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+}
